bc7982b135a6fafdfd55ffc650c25159eba7b647
jhhope1 <jhhope1@naver.com>
2020-11-21 03:12:09 +0900

    multi_label fixed

--- paper/example.py
+++ paper/example.py
@@ -26,4 +26,4 @@ data = csv_data(data_path)
 Plot(data['V1'], data['V2'], 'V', 'V', '$V_1$', '$V_2$', 'example1', save_path, True, 'jpg')
 
 # multi plot
-Plot([data['time'], data['time']] , [data['V1'], data['V2']], 'V', 'V', '$V_1$', '$V_2$', 'example2', save_path, False, 'png', labels = ('V1', 'V2'))
+Plot([data['time'], data['time']] , [data['V1'], data['V2']], 's', 'V', '$t$', '$V_{1}, V_{2}$', 'example2', save_path, False, 'png', labels = ('$V_{1}$', '$V_{2}$'))
